c015f41d4af5e1eb29ed9a2da965cdf89493f7a5
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-21 18:49:29 +0900

    Add useTab extension mode test

--- src/tab.test.ts
+++ src/tab.test.ts
@@ -2,7 +2,7 @@
  * @vitest-environment jsdom
  */
 import { vi, Mock } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { isExtensionMode } from "./env";
 import { useTab } from "./tab";
 
@@ -12,7 +12,34 @@ vi.mock("./env", () => {
   };
 });
 
+const chromeMock = {
+  tabs: {
+    query: vi.fn(),
+  },
+};
+
+vi.stubGlobal("chrome", chromeMock);
+
 describe("useTab", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  test("extension mode", async () => {
+    (isExtensionMode as Mock).mockReturnValueOnce(true);
+    chromeMock.tabs.query.mockResolvedValueOnce([
+      { id: 1, url: "https://example.com" },
+    ]);
+
+    const { result } = renderHook(() => useTab());
+
+    await waitFor(() => {
+      expect(isExtensionMode).toHaveBeenCalled();
+      expect(result.current.id).toBe(1);
+      expect(result.current.url).toBe("https://example.com");
+    });
+  });
+
   test("not extension mode", () => {
     (isExtensionMode as Mock).mockReturnValueOnce(false);
 
